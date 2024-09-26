d9c978896dcd723ad1ad19cbb5941c9bcfc6faf7
Pythmaniac <einsteniano4000@gmail.com>
2024-09-26 22:03:11 +0200

    Aggiunta di alcune funzioni alla propagazione

--- modules/error_propagation.py
+++ modules/error_propagation.py
@@ -1,14 +1,27 @@
-from sympy import symbols, sympify, diff, Abs
+from sympy import symbols, sympify, diff, Abs, cbrt, asin, acos, atan
 from sympy.parsing.sympy_parser import parse_expr
 import numpy as np
 
 class ErrorPropagation:
     def __init__(self):
         self.variables = {}
+        self.np_to_sympy = {
+            'sin': 'sin',
+            'cos': 'cos',
+            'tan': 'tan',
+            'exp': 'exp',
+            'log': 'log',
+            'sqrt': 'sqrt',
+            'cbrt': 'cbrt',
+            'arcsin': 'asin',
+            'arccos': 'acos',
+            'arctan': 'atan',
+            'pi': str(np.pi),
+            'e': str(np.e)
+        }
 
     def add_variable(self, name, value, error):
         try:
-            # Check if the value is a NumPy constant
             if isinstance(value, str) and value.startswith('np.'):
                 value = eval(value)
             if isinstance(error, str) and error.startswith('np.'):
@@ -20,13 +33,8 @@ class ErrorPropagation:
 
     def calculate(self, expression_str):
         try:
-            # Replace numpy functions with their sympy equivalents
-            for np_func in ['sin', 'cos', 'tan', 'exp', 'log', 'sqrt']:
-                expression_str = expression_str.replace(f'np.{np_func}', np_func)
-            
-            # Replace numpy constants
-            expression_str = expression_str.replace('np.pi', str(np.pi))
-            expression_str = expression_str.replace('np.e', str(np.e))
+            for np_func, sympy_func in self.np_to_sympy.items():
+                expression_str = expression_str.replace(f'np.{np_func}', sympy_func)
             
             expr = parse_expr(expression_str)
             var_symbols = {name: symbols(name) for name in self.variables.keys()}
@@ -36,7 +44,7 @@ class ErrorPropagation:
             for var_name, var_data in self.variables.items():
                 partial_derivative = diff(expr, var_symbols[var_name])
                 error_contribution = Abs(partial_derivative.evalf(subs={var_symbols[name]: var['value'] for name, var in self.variables.items()})) * var_data['error']
-                error += error_contribution  # Somma lineare invece di quadratura
+                error += error_contribution
             
             relative_error = error / abs(result)
             percentage_error = relative_error * 100
